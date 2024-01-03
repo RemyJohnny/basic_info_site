21a0b71d30ba180aadd0bd6dba2cdace725fb2b8
Remy Johnny <ewerejohnnyjnr93@gmail.com>
2024-01-03 17:33:48 +0300

    refactored using expressJS

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const http = require("http");
+const PORT = 4000;
+/* const http = require("http");
 const fs = require("fs");
 
-const PORT = 4000;
+
 
 const Server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
@@ -30,4 +31,28 @@ const Server = http.createServer((req, res) => {
 
 Server.listen(PORT, () => {
   console.log("serving at port:" + PORT);
+}); */
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "/404.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`);
 });
